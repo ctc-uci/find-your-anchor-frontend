ded3fa355f26a99eefaf327bd9b6fe6c1a61e055
Zubair Sidhu <sidhuzubair@gmail.com>
2021-11-12 20:12:36 -0800

    Remove bracketSpacing rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import logo from './logo.svg';
 import './App.css';
 
