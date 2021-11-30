6da3e9b5d26fa7fb84a2b7fecb6da14a6582461b
claudeisakeeb <claudeyan2001@gmail.com>
2021-11-30 15:13:37 -0800

    Fix marker popup + Add comments

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,9 +5,14 @@ import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import './Map.css';
 
 const Map = () => {
+  // The activeMarker represents the current marker that is selected (i.e. which popup is shown)
   const [activeMarker, setActiveMarker] = useState(null);
+  // This mapState variable stores the current instance of the map.
+  // This is used to fly to markers when they're clicked
   const [mapState, setMapState] = useState(null);
 
+  // This is sample data for Markers
+  // The only required pieces of data for Markers to show up are the long/lat coordinates
   const markerCoords = [
     {
       id: 'San Diego',
@@ -21,6 +26,7 @@ const Map = () => {
     },
   ];
 
+  // This is the SearchField component used for searching locations
   const SearchField = () => {
     const map = useMap();
 
@@ -46,31 +52,35 @@ const Map = () => {
       scrollWheelZoom
     >
       <TileLayer
+        // Can change this url to display different tilelayers (samples: https://leaflet-extras.github.io/leaflet-providers/preview/)
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-
+      {/* Map the marker data into <Marker /> components */}
       {markerCoords.map(obj => (
         <Marker
           key={obj.id}
           position={obj.coordinates}
           eventHandlers={{
+            // Marker click effect
             click: () => {
               setActiveMarker(obj);
               mapState.flyTo(obj.coordinates, 12);
             },
+            // Marker hover effect
             mouseover: () => {
-              console.log(`hovered over ${obj.id}`);
+              mapState.flyTo(obj.coordinates, 12);
             },
           }}
         />
       ))}
       <SearchField />
+      {/* If a marker is selected, then show the popup for that marker */}
       {activeMarker && (
         <Popup
           className="popup"
           position={activeMarker.coordinates}
-          onClose={setActiveMarker(null)}
+          onClose={() => setActiveMarker(null)}
         >
           <p>{activeMarker.description}</p>
         </Popup>
