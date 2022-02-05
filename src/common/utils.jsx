52d866e7a82708993fe3e54e2e1f3f2d6a00296a
meghakak <meghakak1@gmail.com>
2022-02-05 15:30:52 -0800

    Fix CORS issue.

--- src/common/utils.jsx
+++ src/common/utils.jsx
@@ -5,5 +5,5 @@ const baseURL = 'http://localhost:3001';
 // Import this wherever you make calls to backend.
 export default axios.create({
   baseURL,
-  withCredentials: true,
+  withCredentials: false,
 });
