572099df9f97fdecd2052a2ad1ae1226460cfddb
Jules Labador <juleslabador@gmail.com>
2022-02-05 09:17:52 -0800

    Committing changes

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import BoxApproval from '../../components/BoxApproval/BoxApproval';
 import './AdminDashboard.css';
 
 function AdminDashboard() {
+  // const [data, setData] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   fetch("/")
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data.message));
+  // }, []);
+
+  const FYABackend = axios.create({ baseURL: 'http://localhost:3001', withCredentials: true });
+  console.log('hello');
+  console.log(FYABackend);
+
   return (
     <div className="adminDashBoardDiv">
       <BoxApproval />
