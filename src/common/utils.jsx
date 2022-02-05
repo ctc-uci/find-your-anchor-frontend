85306a9c044dd590afff2d0d5329610c94d86633
meghakak <meghakak1@gmail.com>
2022-02-05 14:12:25 -0800

    Fix base URL for axios instance.

--- src/common/utils.jsx
+++ src/common/utils.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001';
+const baseURL = 'http://localhost:3001';
 
 // Import this wherever you make calls to backend.
 export default axios.create({
-  baseUrl,
+  baseURL,
   withCredentials: true,
 });
