8a93a1054fbb84a72eca1419713f0b8d87b9fde3
Kyle Pan <kylerpan691@gmail.com>
2022-02-11 18:41:05 -0800

    used react datapicker and fixed range checker

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
+import ExportCSV from './pages/ExportCSV/ExportCSV';
 
 import Map from './components/Map/Map';
-import ExportCSV from './components/ExportCSV/ExportCSV';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/ExportCSV/ExportCSV.jsx
+++ src/pages/ExportCSV/ExportCSV.jsx
@@ -1,115 +1,87 @@
 import React from 'react';
 import { ChakraProvider, Select, Button, Input, FormLabel } from '@chakra-ui/react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import './ExportCSV.css';
 
 function ExportCSV() {
-  const [sort, setSort] = React.useState('ascend');
-  const [boxes, setBoxes] = React.useState('all');
-  const [date, setDate] = React.useState('all');
-  const [launch, setLaunch] = React.useState('both');
-  const [zip, setZip] = React.useState('all');
-  const [boxesCustom, setBoxesCustom] = React.useState('');
-  const [dateCustom, setDateCustom] = React.useState('');
-  const [startCustom, setStartCustom] = React.useState('');
-  const [endCustom, setEndCustom] = React.useState('');
-  const [zipCustom, setZipCustom] = React.useState('');
+  const [sortOption, setSortOption] = React.useState('ascend');
+  const [boxesOption, setBoxesOption] = React.useState('all');
+  const [dateOption, setDate] = React.useState('all');
+  const [launchOption, setLaunchOption] = React.useState('both');
+  const [zipOption, setZipOption] = React.useState('all');
+  const [customBoxes, setCustomBoxes] = React.useState('');
+  const [customDate, setCustomDate] = React.useState(new Date());
+  const [customStartDate, setCustomStartDate] = React.useState('');
+  const [customEndDate, setCustomEndDate] = React.useState('');
+  const [customZip, setCustomZip] = React.useState('');
 
   // Function to check valid range
   function checkRange(value) {
-    if (value === '') return false;
-    if (value.includes('-')) {
+    if (value === '') {
+      return false;
+    }
+    if (value.includes(',')) {
+      const ranges = value.split(', ');
+      for (let i = 0; i < ranges.length; i += 1) {
+        if (ranges[i].includes('-')) {
+          const numbers = ranges[i].split('-');
+          if (!Number.isNaN(Number(numbers[0])) && !Number.isNaN(Number(numbers[1]))) {
+            if (numbers[0] > numbers[1]) {
+              return true;
+            }
+          } else if (Number.isNaN(Number(numbers[0])) || Number.isNaN(Number(numbers[1]))) {
+            return true;
+          }
+        } else {
+          return Number.isNaN(Number(ranges[i]));
+        }
+      }
+    } else if (value.includes('-')) {
       const numbers = value.split('-');
       if (!Number.isNaN(Number(numbers[0])) && !Number.isNaN(Number(numbers[1]))) {
-        if (numbers[0] < numbers[1]) {
-          return false;
-        }
-      }
-    } else {
-      if (!Number.isNaN(Number(value))) return false;
-      return true;
-    }
-    return true;
-  }
-
-  // Function to check the valid date
-  function checkDate(value) {
-    const dates = {
-      1: 31,
-      2: 28,
-      3: 31,
-      4: 30,
-      5: 31,
-      6: 30,
-      7: 31,
-      8: 31,
-      9: 30,
-      10: 31,
-      11: 30,
-      12: 31,
-    };
-    if (value === '') return false;
-    const numbers = value.split('/');
-    if (
-      Number.isNaN(Number(numbers[0])) ||
-      Number.isNaN(Number(numbers[1])) ||
-      Number.isNaN(Number(numbers[2]))
-    )
-      return true;
-    if (numbers[2].length !== 2) return true;
-    if (numbers[0] === '2' && numbers[2] % 4 === 0) dates[2] += 1;
-    if (
-      Object.keys(dates).includes(numbers[0]) &&
-      numbers[1] >= 1 &&
-      numbers[1] <= dates[numbers[0]] &&
-      numbers[2] != null
-    )
-      return false;
-    return true;
-  }
-
-  // Function to check if start date is less than end date
-  function checkDateLess(value1, value2) {
-    if (checkDate(value1) || checkDate(value2)) return true;
-    const start = value1.split('/');
-    const end = value2.split('/');
-    if (end[2] - start[2] < 0) {
-      return true;
-    }
-    if (end[2] - start[2] === 0) {
-      if (end[1] - start[1] < 0) {
-        return true;
-      }
-      if (end[1] - start[1] === 0) {
-        if (end[0] - start[0] < 0) {
+        if (numbers[0] > numbers[1]) {
           return true;
         }
+      } else if (Number.isNaN(Number(numbers[0])) || Number.isNaN(Number(numbers[1]))) {
+        return true;
       }
+    } else {
+      return Number.isNaN(Number(value));
     }
     return false;
   }
 
   // Function to check valid zip code
   function checkZip(value) {
-    if (value === '') return false;
-    if (value.length === 5 && !Number.isNaN(Number(value))) return false;
+    if (value === '') {
+      return false;
+    }
+    if (value.length === 5 && !Number.isNaN(Number(value))) {
+      return false;
+    }
     return true;
   }
 
   return (
     <ChakraProvider>
       <div className="export-csv-container">
-        <div className="left-column">
+        <div className="export-table-preview">
           <img alt="sample" src="https://pngimg.com/uploads/box/box_PNG49.png" />
         </div>
-        <div className="right-column">
+        <div className="filter-section">
           <div className="two-text-header">
             <p style={{ fontSize: '36px', fontWeight: '700' }}>Export CSV</p>
             <p style={{ fontSize: '26px' }}>30 boxes</p>
           </div>
           <div className="section">
             <p className="option-header">Sort By</p>
-            <Select fontSize="20px" value={sort} onChange={e => setSort(e.target.value)}>
+            <Select
+              fontSize="20px"
+              value={sortOption}
+              onChange={e => setSortOption(e.target.value)}
+            >
               <option value="ascend-box-num">Ascending Box Number</option>
               <option value="descend-box-num">Descending Box Number</option>
               <option value="chronologic">Chronologically</option>
@@ -126,22 +98,22 @@ function ExportCSV() {
                   <Select
                     fontSize="20px"
                     width="300px"
-                    value={boxes}
-                    onChange={e => setBoxes(e.target.value)}
+                    value={boxesOption}
+                    onChange={e => setBoxesOption(e.target.value)}
                   >
                     <option value="all">All</option>
                     <option value="custom">Custom</option>
                   </Select>
-                  {boxes === 'custom' && (
+                  {boxesOption === 'custom' && (
                     <div>
                       <Input
-                        value={boxesCustom}
+                        value={customBoxes}
                         className="custom-text"
                         placeholder="e.g. 1-9, 6, 12"
-                        isInvalid={checkRange(boxesCustom)}
-                        onChange={e => setBoxesCustom(e.target.value)}
+                        isInvalid={checkRange(customBoxes)}
+                        onChange={e => setCustomBoxes(e.target.value)}
                       />
-                      {checkRange(boxesCustom) && (
+                      {checkRange(customBoxes) && (
                         <p className="error-message">Invalid page range, e.g. 1-9, 6, 12</p>
                       )}
                     </div>
@@ -155,60 +127,54 @@ function ExportCSV() {
                     <Select
                       fontSize="20px"
                       width="300px"
-                      value={date}
+                      value={dateOption}
                       onChange={e => setDate(e.target.value)}
                     >
                       <option value="all">All</option>
                       <option value="single">Single Day</option>
                       <option value="range">Range</option>
                     </Select>
-                    {date === 'single' && (
-                      <div>
-                        <Input
-                          value={dateCustom}
-                          className="custom-text"
-                          placeholder="MM/DD/YY"
-                          isInvalid={checkDate(dateCustom)}
-                          onChange={e => setDateCustom(e.target.value)}
-                        />
-                        {checkDate(dateCustom) && (
-                          <p className="error-message">Invalid date, use MM/DD/YY format</p>
-                        )}
-                      </div>
+                    {dateOption === 'single' && (
+                      <DatePicker
+                        className="single-date-picker"
+                        placeholderText="MM/DD/YY"
+                        selected={customDate}
+                        onChange={date => setCustomDate(date)}
+                      />
                     )}
-                    {date === 'range' && (
+                    {dateOption === 'range' && (
                       <div>
                         <div className="custom-range-boxes">
                           <div>
                             <FormLabel fontSize="16px" className="date-label" htmlFor="from-date">
                               Start
                             </FormLabel>
-                            <Input
-                              value={startCustom}
-                              id="from-date"
-                              className="custom-range"
-                              placeholder="MM/DD/YY"
-                              isInvalid={checkDateLess(startCustom, endCustom)}
-                              onChange={e => setStartCustom(e.target.value)}
+                            <DatePicker
+                              className="range-date-picker"
+                              placeholderText="MM/DD/YY"
+                              selected={customStartDate}
+                              onChange={date => setCustomStartDate(date)}
+                              selectsStart
+                              startDate={customStartDate}
+                              endDate={customEndDate}
                             />
                           </div>
                           <div>
                             <FormLabel fontSize="16px" className="date-label" htmlFor="from-date">
                               End
                             </FormLabel>
-                            <Input
-                              value={endCustom}
-                              id="to-date"
-                              className="custom-range"
-                              placeholder="MM/DD/YY"
-                              isInvalid={checkDateLess(startCustom, endCustom)}
-                              onChange={e => setEndCustom(e.target.value)}
+                            <DatePicker
+                              className="range-date-picker"
+                              placeholderText="MM/DD/YY"
+                              selected={customEndDate}
+                              onChange={date => setCustomEndDate(date)}
+                              selectsEnd
+                              startDate={customStartDate}
+                              endDate={customEndDate}
+                              minDate={customStartDate}
                             />
                           </div>
                         </div>
-                        {checkDateLess(startCustom, endCustom) && (
-                          <p className="error-message">Invalid date, use MM/DD/YY format</p>
-                        )}
                       </div>
                     )}
                   </div>
@@ -219,8 +185,8 @@ function ExportCSV() {
                 <Select
                   fontSize="20px"
                   width="300px"
-                  value={launch}
-                  onChange={e => setLaunch(e.target.value)}
+                  value={launchOption}
+                  onChange={e => setLaunchOption(e.target.value)}
                 >
                   <option value="both">Both</option>
                   <option value="organically">Organically</option>
@@ -233,22 +199,22 @@ function ExportCSV() {
                   <Select
                     fontSize="20px"
                     width="300px"
-                    value={zip}
-                    onChange={e => setZip(e.target.value)}
+                    value={zipOption}
+                    onChange={e => setZipOption(e.target.value)}
                   >
                     <option value="all">All</option>
                     <option value="custom">Custom</option>
                   </Select>
-                  {zip === 'custom' && (
+                  {zipOption === 'custom' && (
                     <div>
                       <Input
-                        value={zipCustom}
+                        value={customZip}
                         className="custom-text"
                         placeholder="e.g. 96162, 91007"
-                        isInvalid={checkZip(zipCustom)}
-                        onChange={e => setZipCustom(e.target.value)}
+                        isInvalid={checkZip(customZip)}
+                        onChange={e => setCustomZip(e.target.value)}
                       />
-                      {checkZip(zipCustom) && (
+                      {checkZip(customZip) && (
                         <p className="error-message">Invalid zipcode, use e.g. 92602, 92123</p>
                       )}
                     </div>
