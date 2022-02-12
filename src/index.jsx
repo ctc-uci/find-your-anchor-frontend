7fbb1420b11e95fbf667b3f272e5cf62aa65fa94
austruong <truong.austin1@gmail.com>
2022-02-11 20:06:10 -0800

    Created Empty AdminProfilePage

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
+import AdminProfilePage from './pages/AdminProfilePage/AdminProfilePage';
 
 import Map from './components/Map/Map';
 
@@ -13,6 +14,7 @@ ReactDOM.render(
       <Routes>
         <Route exact path="/" element={<Map />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/profile" element={<AdminProfilePage />} />
         <Route path="/login" element={<App />} />
         <Route path="/dropoff-form" element={<App />} />
         <Route path="/pickup-form" element={<App />} />
