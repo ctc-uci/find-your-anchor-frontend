dd3a2b45347d7de1b59a8ba2ba6bd8b1e16e89c5
Kyle Pan <kylerpan691@gmail.com>
2022-02-12 21:39:55 -0800

    updated admin interface with review submission button

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { ChakraProvider, Button } from '@chakra-ui/react';
+
 import Map from '../../components/Map/Map';
 import BoxApproval from '../../components/BoxApproval/BoxApproval';
 import NavBar from '../../components/NavBar/NavBar';
 import './AdminDashboard.css';
 
 function AdminDashboard() {
+  // const [reviewSubmission, setReviewSubmission] = useState(false);
+  // const [closeButton, setCloseButton] = useState(false);
+
+  function setReviewSubmission() {
+    const sideBarComponent = document.getElementsByClassName('side-bar')[0];
+    const mapComponent = document.getElementsByClassName('map')[0];
+    const reviewSubmissionComponent = document.getElementsByClassName(
+      'review-submission-button',
+    )[0];
+    sideBarComponent.style.display = 'block';
+    mapComponent.style.width = '80vw';
+    reviewSubmissionComponent.style.display = 'none';
+  }
+
+  function setCloseButton() {
+    const sideBarComponent = document.getElementsByClassName('side-bar')[0];
+    const mapComponent = document.getElementsByClassName('map')[0];
+    const reviewSubmissionComponent = document.getElementsByClassName(
+      'review-submission-button',
+    )[0];
+    sideBarComponent.style.display = 'none';
+    mapComponent.style.width = '100vw';
+    reviewSubmissionComponent.style.display = 'block';
+  }
+
   return (
     <div className="admin-dashboard-container">
       <div className="admin-nav-bar">
@@ -13,10 +40,27 @@ function AdminDashboard() {
       <div className="side-bar-and-map-container">
         <div className="side-bar">
           <BoxApproval />
+          <Button
+            variant="link"
+            colorScheme="white"
+            className="close-button"
+            onClick={() => setCloseButton()}
+          >
+            Close
+          </Button>
         </div>
         <div className="map">
           <Map />
         </div>
+        <ChakraProvider>
+          <Button
+            colorScheme="blue"
+            className="review-submission-button"
+            onClick={() => setReviewSubmission()}
+          >
+            Review Submission
+          </Button>
+        </ChakraProvider>
       </div>
     </div>
   );
