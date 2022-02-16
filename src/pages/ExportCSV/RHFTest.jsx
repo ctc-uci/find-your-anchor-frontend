16fba9235b4e1c0e4236af6436407810cdf10220
Zubair Sidhu <sidhuzubair@gmail.com>
2022-02-15 16:14:01 -0800

    Add custom validation function

--- src/pages/ExportCSV/RHFTest.jsx
+++ src/pages/ExportCSV/RHFTest.jsx
@@ -7,10 +7,33 @@ import { ChakraProvider, FormControl, FormLabel, Input, Button } from '@chakra-u
 
 import './ExportCSV.css';
 
+function isPalindrome(message) {
+  return this.test('isPalindrome', message, function palindromeCheck(value) {
+    const { path, createError } = this;
+
+    const re = /[\W_]/g;
+    const lowRegStr = value.toLowerCase().replace(re, '');
+    const reverseStr = lowRegStr.split('').reverse().join('');
+
+    if (reverseStr === lowRegStr) {
+      return true;
+    }
+
+    return createError({ path, message: message ?? 'Not a palindrome' });
+  });
+}
+yup.addMethod(yup.mixed, 'isPalindrome', isPalindrome);
+
 const schema = yup
   .object({
     requiredText: yup.string().required(),
-    requiredNum: yup.number().positive().integer().required(),
+    requiredNum: yup
+      .number()
+      .typeError('requiredNum must be a positive number')
+      .positive()
+      .integer()
+      .required(),
+    palindrome: yup.mixed().isPalindrome(),
   })
   .required();
 
@@ -41,6 +64,13 @@ const RHFTest = () => {
             <Input id="requiredNum" placeholder="Enter Number" {...register('requiredNum')} />
             <p className="error-message">{errors.requiredNum?.message}</p>
           </FormControl>
+
+          <FormControl isInvalid={errors?.palindrome}>
+            <FormLabel htmlFor="palindrome">Enter a palindrome</FormLabel>
+            <Input id="palindrome" placeholder="e.g. racecar" {...register('palindrome')} />
+            <p className="error-message">{errors.palindrome?.message}</p>
+          </FormControl>
+
           <Button type="submit">Submit</Button>
         </form>
       </div>
