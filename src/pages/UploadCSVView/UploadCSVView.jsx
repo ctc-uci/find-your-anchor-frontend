bdff5be52cd4d3db8b324068718f3ed76bf377d7
Ashley Liu <aslorange61@gmail.com>
2022-02-19 08:54:06 -0800

    create dummy data in UploadCSVPage

--- src/pages/UploadCSVView/UploadCSVView.jsx
+++ src/pages/UploadCSVView/UploadCSVView.jsx
@@ -1,7 +1,41 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './UploadCSVView.css';
 
 const UploadCSVView = () => {
-  return <div>hi</div>;
+  const formDatas = [
+    {
+      date: '09/28/2022',
+      boxNumber: '123',
+      zipCode: '13252',
+      launchedOrganically: 'yes',
+    },
+    {
+      date: '09/28/2022',
+      boxNumber: '123',
+      zipCode: '13252',
+      launchedOrganically: 'yes',
+    },
+  ];
+
+  return (
+    <ChakraProvider>
+      <div>
+        {formDatas.map(formData => {
+          const id = uuidv4();
+          return (
+            <div key={id} className="csv-row">
+              <p>{formData.date}</p>
+              <p>{formData.boxNumber}</p>
+              <p>{formData.zipCode}</p>
+              <p>{formData.launchedOrganically}</p>
+            </div>
+          );
+        })}
+      </div>
+    </ChakraProvider>
+  );
 };
 
 export default UploadCSVView;
