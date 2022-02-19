ebe9649622a3474e1bccaa83d8cd3c7056e7b278
Ashley Liu <aslorange61@gmail.com>
2022-02-19 15:26:21 -0800

    fix ErrorModal css

--- src/components/UploadCSV/ErrorModal/ErrorModal.jsx
+++ src/components/UploadCSV/ErrorModal/ErrorModal.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
-import { Button } from '@chakra-ui/react';
+import { Button, ButtonGroup, Text } from '@chakra-ui/react';
 import './ErrorModal.css';
+import ErrorIcon from '../../../assets/error.png';
 
 const ErrorModal = ({ CSVFileName, setIsUploadingNewFile, uploadErrors }) => {
   const uploadNewFile = () => {
@@ -14,9 +15,9 @@ const ErrorModal = ({ CSVFileName, setIsUploadingNewFile, uploadErrors }) => {
   };
 
   return (
-    <div className="error-modal-container">
-      <div className="modal-icon error-modal-icon" />
-      <p className="error-modal-text">Oops!</p>
+    <div className="error-modal-content">
+      <img src={ErrorIcon} alt="Icon" className="error-modal-icon" />
+      <Text className="error-modal-text">Oops!</Text>
       <p className="error-modal-text">There was an error with the uploaded file:</p>
       <p className="error-modal-file-name">{CSVFileName}</p>
       <div className="error-modal-errors">
@@ -29,14 +30,14 @@ const ErrorModal = ({ CSVFileName, setIsUploadingNewFile, uploadErrors }) => {
           );
         })}
       </div>
-      <div className="error-modal-buttons">
+      <ButtonGroup className="error-modal-buttons">
         <Button size="md" colorScheme="blackAlpha" onClick={uploadNewFile}>
           Upload New File
         </Button>
         <Button size="md" colorScheme="teal" onClick={e => viewFile(e)}>
           Edit/View File
         </Button>
-      </div>
+      </ButtonGroup>
     </div>
   );
 };
