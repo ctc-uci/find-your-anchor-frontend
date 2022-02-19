e97a64939ca432f84e82d3f9ffa9cc7d6516ae30
austruong <truong.austin1@gmail.com>
2022-02-19 14:23:43 -0800

    Added Vector.png and modified css and jsx of AdminProfilePage

--- src/pages/AdminProfilePage/AdminProfilePage.jsx
+++ src/pages/AdminProfilePage/AdminProfilePage.jsx
@@ -19,15 +19,28 @@ const AdminProfilePage = () => {
   return (
     <ChakraProvider>
       <div>
-        <Button colorScheme="red" onClick={onOpenDeleteModal}>
-          Delete Account
-        </Button>
-        <DeleteAccountModal isOpen={isOpenDeleteModal} onClose={onCloseDeleteModal} />
-        <Button onClick={onOpen} color="white" bg="#1F2F38">
-          Share Registration Link
-        </Button>
-        <SendLinkModal isOpen={isOpen} onClose={onClose} />
+        <div className="sub-header">
+          <Button onClick={onOpen} color="white" bg="#1F2F38" className="share-registration-box">
+            Share Registration Link
+          </Button>
+          <SendLinkModal isOpen={isOpen} onClose={onClose} />
+
+          <div className="welcome-header" colorScheme="blue">
+            <h1>WELCOME!</h1>
+          </div>
+
+          <div className="logout-button">
+            <Button colorScheme="teal" onClick={null}>
+              Logout
+            </Button>
+          </div>
+        </div>
       </div>
+
+      <Button colorScheme="red" onClick={onOpenDeleteModal}>
+        Delete Account
+      </Button>
+      <DeleteAccountModal isOpen={isOpenDeleteModal} onClose={onCloseDeleteModal} />
     </ChakraProvider>
   );
 };
