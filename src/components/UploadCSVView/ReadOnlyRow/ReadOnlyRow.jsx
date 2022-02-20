63132ca3a8c75bad9c697a639b86cd99e8e53864
Ashley Liu <aslorange61@gmail.com>
2022-02-19 22:25:01 -0800

    create rows for UploadCSVView

---
+++ src/components/UploadCSVView/ReadOnlyRow/ReadOnlyRow.jsx
@@ -0,0 +1,33 @@
+// eslint-disable react/forbid-prop-types
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ReadOnlyRow.css';
+import EditIcon from '../../../assets/edit.png';
+import DeleteIcon from '../../../assets/delete.png';
+
+const ReadOnlyRow = ({ data, editRow, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td>{data.date}</td>
+      <td>{data.boxNumber}</td>
+      <td>{data.zipCode}</td>
+      <td>{data.launchedOrganically}</td>
+      <td>
+        <button type="button" onClick={() => handleDeleteClick(data.id)}>
+          <img src={DeleteIcon} alt="Edit Icon" className="delete-icon" />
+        </button>
+        <button type="button" onClick={e => editRow(e, data)}>
+          <img src={EditIcon} alt="Edit Icon" className="edit-icon" />
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+ReadOnlyRow.propTypes = {
+  data: PropTypes.objectOf(PropTypes.object).isRequired,
+  editRow: PropTypes.func.isRequired,
+  handleDeleteClick: PropTypes.func.isRequired,
+};
+
+export default ReadOnlyRow;
--- src/pages/UploadCSVView/UploadCSVView.jsx
+++ src/pages/UploadCSVView/UploadCSVView.jsx
@@ -1,18 +1,33 @@
-import { ChakraProvider, Input } from '@chakra-ui/react';
-import { EditIcon, CheckIcon } from '@chakra-ui/icons';
+/* eslint-disable prefer-object-spread */
+
+import { ChakraProvider } from '@chakra-ui/react';
+// import { CheckIcon } from '@chakra-ui/icons';
 import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import React from 'react';
+// import { v4 as uuidv4 } from 'uuid';
 import './UploadCSVView.css';
+// import EditIcon from '../../assets/edit.png';
+import ReadOnlyRow from '../../components/UploadCSVView/ReadOnlyRow/ReadOnlyRow';
+import EditableRow from '../../components/UploadCSVView/EditableRow/EditableRow';
 
 const UploadCSVView = () => {
-  const formDatas = [
+  const rows = [
     {
+      id: 1,
       date: '09/28/2022',
       boxNumber: '123',
       zipCode: '13252',
       launchedOrganically: 'yes',
     },
     {
+      id: 2,
+      date: '09/28/2022',
+      boxNumber: '123',
+      zipCode: '13252',
+      launchedOrganically: 'yes',
+    },
+    {
+      id: 3,
       date: '09/28/2022',
       boxNumber: '123',
       zipCode: '13252',
@@ -20,62 +35,97 @@ const UploadCSVView = () => {
     },
   ];
 
-  const [edit, setEdit] = useState(true);
+  // const [edit, setEdit] = useState(true);
+  const [formDatas, setFormData] = useState(rows);
+  const [editId, setEditId] = useState(null);
+
+  const [editFormData, setEditFormData] = useState({
+    date: '',
+    boxNumber: '',
+    zipCode: '',
+    launchedOrganically: '',
+  });
+  // const [contacts, setContacts] = useState(formData);
+
+  const editRow = (e, data) => {
+    e.preventDefault();
+    setEditId(data.id);
+
+    const formValues = {
+      date: data.date,
+      boxNumber: data.boxNumber,
+      zipCode: data.zipCode,
+      launchedOrganically: data.launchedOrganically,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleEditFormChange = e => {
+    e.preventDefault();
+    setEditFormData(Object.assign({}, editFormData, { [e.target.name]: e.target.value }));
+  };
+
+  const handleEditFormSubmit = e => {
+    e.preventDefault();
+    const editedRow = {
+      id: editId,
+      date: editFormData.date,
+      boxNumber: editFormData.boxNumber,
+      zipCode: editFormData.zipCode,
+      launchedOrganically: editFormData.launchedOrganically,
+    };
+
+    const newFormData = [...formDatas];
+    const index = formDatas.findIndex(data => data.id === editId); // get index of the row that we are editing
+    newFormData[index] = editedRow; // update the array at index
+    setEditId(null);
+    setFormData(newFormData);
+  };
+
+  const handleDeleteClick = rowId => {
+    const newFormData = [...formDatas];
+    const index = formDatas.findIndex(data => data.id === rowId);
+    newFormData.splice(index, 1);
+    setFormData(newFormData);
+  };
 
   return (
     <ChakraProvider>
-      <div className="csv-table">
-        {/* <div className="csv-header-row">
-          <th>Date</th>
-          <th>Box Number</th>
-          <th>Zip Code</th>
-          <th>Launched Organically</th>
-          <th>&nbsp;</th>
-        </div> */}
-        {formDatas.map(formData => {
-          const id = uuidv4();
-          return (
-            <div key={id}>
-              {edit ? (
-                <div className="csv-row">
-                  <p>{formData.date}</p>
-                  <p>{formData.boxNumber}</p>
-                  <p>{formData.zipCode}</p>
-                  <p>{formData.launchedOrganically}</p>
-                  <EditIcon onClick={() => setEdit(!edit)} />
-                </div>
-              ) : (
-                <div className="csv-row">
-                  <Input
-                    id="boxNumber"
-                    placeholder="12345"
-                    name="boxNumber"
-                    // value={formData.boxNumber}
-                  />
-                  <Input
-                    id="boxNumber"
-                    placeholder="12345"
-                    name="boxNumber"
-                    // value={formData.boxNumber}
-                  />
-                  <Input
-                    id="boxNumber"
-                    placeholder="12345"
-                    name="boxNumber"
-                    // value={formData.boxNumber}
-                  />
-                  <Input
-                    id="boxNumber"
-                    placeholder="12345"
-                    name="boxNumber"
-                    // value={formData.boxNumber}
-                  />
-                  <CheckIcon onClick={() => setEdit(!edit)} />
-                </div>
-              )}
-            </div>
-          );
-        })}
+      <div className="app-container">
+        <form onSubmit={handleEditFormSubmit}>
+          <table className="csv-table">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Box Number</th>
+                <th>Zip Code</th>
+                <th>Launched Organically</th>
+                <th>&nbsp;</th>
+              </tr>
+            </thead>
+            <tbody>
+              {formDatas.map(data => {
+                return (
+                  <>
+                    {editId === data.id ? (
+                      <EditableRow
+                        editFormData={editFormData}
+                        handleEditFormChange={handleEditFormChange}
+                      />
+                    ) : (
+                      <ReadOnlyRow
+                        data={data}
+                        editRow={editRow}
+                        handleDeleteClick={handleDeleteClick}
+                      />
+                    )}
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+        </form>
       </div>
     </ChakraProvider>
   );
