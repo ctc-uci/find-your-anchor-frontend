17c9cc08ffe6cf3e28e0fcdce3e963df14f66d39
austruong <truong.austin1@gmail.com>
2022-02-19 20:39:37 -0800

    Added Admin Profile Page

--- src/pages/AdminProfilePage/AdminProfilePage.jsx
+++ src/pages/AdminProfilePage/AdminProfilePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { ChakraProvider, Button, useDisclosure } from '@chakra-ui/react';
+import { ChakraProvider, Button, useDisclosure, Input, Image } from '@chakra-ui/react';
 
+import './AdminProfilePage.css';
 import DeleteAccountModal from './DeleteAccountModal/DeleteAccountModal';
 import SendLinkModal from './SendLinkModal/SendLinkModal';
+import NavBar from '../../components/NavBar/NavBar';
+import FYALogoLarge from '../../assets/fya-logo-large.svg';
+import PenImage from '../../assets/Vector.png';
 
 // TODO:
 // - Remove ChakraProvider once provider is moved to index.js
@@ -18,29 +22,92 @@ const AdminProfilePage = () => {
 
   return (
     <ChakraProvider>
-      <div>
-        <div className="sub-header">
-          <Button onClick={onOpen} color="white" bg="#1F2F38" className="share-registration-box">
-            Share Registration Link
+      <div className="navigation-bar-container">
+        <NavBar />
+      </div>
+      <div className="sub-header">
+        <Button
+          onClick={onOpen}
+          color="white"
+          className="share-registration-box"
+          height="41px"
+          width="273px"
+          borderRadius="15px"
+          bg="#1F2F38"
+          fontSize="20px"
+        >
+          Share Registration Link
+        </Button>
+        <SendLinkModal isOpen={isOpen} onClose={onClose} />
+
+        <div className="welcome-header" colorScheme="blue">
+          <h1>WELCOME!</h1>
+        </div>
+
+        <div className="logout-button">
+          <Button
+            onClick={null}
+            colorScheme="teal"
+            height="57px"
+            width="184px"
+            borderRadius="15px"
+            bg="#345E80"
+            fontSize="27px"
+          >
+            Logout
           </Button>
-          <SendLinkModal isOpen={isOpen} onClose={onClose} />
-
-          <div className="welcome-header" colorScheme="blue">
-            <h1>WELCOME!</h1>
-          </div>
-
-          <div className="logout-button">
-            <Button colorScheme="teal" onClick={null}>
-              Logout
-            </Button>
-          </div>
         </div>
       </div>
 
-      <Button colorScheme="red" onClick={onOpenDeleteModal}>
-        Delete Account
-      </Button>
-      <DeleteAccountModal isOpen={isOpenDeleteModal} onClose={onCloseDeleteModal} />
+      <div className="fya-logo-large">
+        <img boxSize="100px" src={FYALogoLarge} alt="Find Your Anchor Logo" />
+      </div>
+
+      <div className="background-box">
+        <div className="profile-name">
+          <div className="first-name-container">
+            <h1>First Name:</h1>
+            <div className="first-input-container">
+              <Input
+                bg="#FFFFFF"
+                className="first-name-input"
+                width="404px"
+                height="49px"
+                top="5px"
+              />
+              <Image boxSize="27.75" className="pen-image" src={PenImage} alt="text input" />
+            </div>
+          </div>
+
+          <div className="last-name-container">
+            <h1>Last Name:</h1>
+            <div className="last-input-container">
+              <Input
+                bg="#FFFFFF"
+                className="last-name-input"
+                width="404px"
+                height="49px"
+                top="5px"
+              />
+              <Image boxSize="27.75" className="pen-image" src={PenImage} alt="text input" />
+            </div>
+          </div>
+        </div>
+
+        <div className="email-container">
+          <h1>Email Address:</h1>
+          <div className="email-input-container">
+            <Input bg="#FFFFFF" className="email-input" width="861px" height="49px" top="11px" />
+          </div>
+        </div>
+
+        <div className="delete-account-button">
+          <Button colorScheme="red" onClick={onOpenDeleteModal}>
+            Delete Account
+          </Button>
+          <DeleteAccountModal isOpen={isOpenDeleteModal} onClose={onCloseDeleteModal} />
+        </div>
+      </div>
     </ChakraProvider>
   );
 };
