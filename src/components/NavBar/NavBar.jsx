153089d8c96c64b59efd426f151acb9f5330e119
Zubair Sidhu <sidhuzubair@gmail.com>
2022-02-26 08:16:34 -0800

    Make navbar styles a css module

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './NavBar.css';
+import styles from './NavBar.module.css';
 import FYALogo from '../../assets/fya-logo.png';
 
 function NavBar() {
   return (
-    <div className="nav-bar">
-      <div className="fya-logo">
+    <div className={styles['nav-bar']}>
+      <div className={styles['fya-logo']}>
         <img src={FYALogo} alt="Find Your Anchor Logo" />
       </div>
-      <div className="navbar-buttons-and-account">
-        <Link className="navbar-buttons" to="/add-box-form">
+      <div className={styles['navbar-buttons-and-account']}>
+        <Link className={styles['navbar-buttons']} to="/add-box-form">
           Add Box
         </Link>
-        <Link className="navbar-buttons" to="/upload-csv">
+        <Link className={styles['navbar-buttons']} to="/upload-csv">
           Upload CSV
         </Link>
-        <Link className="navbar-buttons" to="/export-csv">
+        <Link className={styles['navbar-buttons']} to="/export-csv">
           Export CSV
         </Link>
         <Link to="/profile">
-          <div className="navbar-account">
-            <span className="navbar-account-circle" />
-            <p className="navbar-account-name">SA</p>
+          <div className={styles['navbar-account']}>
+            <span className={styles['navbar-account-circle']} />
+            <p className={styles['navbar-account-name']}>SA</p>
           </div>
         </Link>
       </div>
