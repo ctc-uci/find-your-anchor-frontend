b517c12c3ec5dd88b931ba7b3f7e4de710076d0f
Ashley Liu <aslorange61@gmail.com>
2022-02-27 10:48:40 -0800

    remove UploadCSVTemp and add UploadCSV to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ChakraProvider, Button, useDisclosure } from '@chakra-ui/react';
+import UploadCSV from '../UploadCSV/UploadCSV';
 
 import './NavBar.css';
 import FYALogo from '../../assets/fya-logo.png';
 
 function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div className="nav-bar">
-      <div className="fya-logo">
-        <img src={FYALogo} alt="Find Your Anchor Logo" />
+    <ChakraProvider>
+      <div className="nav-bar">
+        <div className="fya-logo">
+          <img src={FYALogo} alt="Find Your Anchor Logo" />
+        </div>
+        <div className="navbar-buttons-and-account">
+          <Link className="navbar-buttons" to="/add-box-form">
+            Add Box
+          </Link>
+          <Button variant="unstyled" onClick={onOpen} className="navbar-buttons">
+            Upload CSV
+            <UploadCSV isOpen={isOpen} onClose={onClose} />
+          </Button>
+          <Link className="navbar-buttons" to="/export-csv">
+            Export CSV
+          </Link>
+          <Link to="/profile">
+            <div className="navbar-account">
+              <span className="navbar-account-circle" />
+              <p className="navbar-account-name">SA</p>
+            </div>
+          </Link>
+        </div>
       </div>
-      <div className="navbar-buttons-and-account">
-        <Link className="navbar-buttons" to="/add-box-form">
-          Add Box
-        </Link>
-        <Link className="navbar-buttons" to="/upload-csv">
-          Upload CSV
-        </Link>
-        <Link className="navbar-buttons" to="/export-csv">
-          Export CSV
-        </Link>
-        <Link to="/profile">
-          <div className="navbar-account">
-            <span className="navbar-account-circle" />
-            <p className="navbar-account-name">SA</p>
-          </div>
-        </Link>
-      </div>
-    </div>
+    </ChakraProvider>
   );
 }
 
