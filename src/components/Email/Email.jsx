4dc895ff27bf51bf6b86e537351f36d865fe623f
jennymylee <jennylee038@gmail.com>
2022-03-02 15:58:09 -0800

    changed email template

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Email, Item, A } from 'react-html-email';
+import { Email, Item } from 'react-html-email';
 
 export default function MyEmail({ name }) {
   return (
     <Email title="link">
-      <Item>
-        Hello {name}
-        <A style={{ paddingLeft: 10 }} href="/blog/">
-          Click me!
-        </A>
-      </Item>
+      <Item> Hello {name}, your box has been approved! </Item>
     </Email>
   );
 }
