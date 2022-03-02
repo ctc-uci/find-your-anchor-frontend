8b089688daf4a91468682120428f8fb9ac0b73d1
Zubair Sidhu <sidhuzubair@gmail.com>
2022-03-01 18:19:25 -0800

    Add optional className prop to ModalBody of CommonModal

--- src/common/CommonModal/CommonModal.jsx
+++ src/common/CommonModal/CommonModal.jsx
@@ -9,24 +9,30 @@ import {
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-const CommonModal = ({ isOpen, onClose, children }) => {
+const CommonModal = ({ isOpen, onClose, children, modalClassName }) => {
   return (
     <ChakraProvider>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent minHeight="500px" minWidth="700px">
           <ModalCloseButton />
-          <ModalBody>{children}</ModalBody>
+          <ModalBody className={modalClassName}>{children}</ModalBody>
         </ModalContent>
       </Modal>
     </ChakraProvider>
   );
 };
 
+CommonModal.defaultProps = {
+  modalClassName: '',
+};
+
 CommonModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  modalClassName: PropTypes.string,
 };
 
 export default CommonModal;
