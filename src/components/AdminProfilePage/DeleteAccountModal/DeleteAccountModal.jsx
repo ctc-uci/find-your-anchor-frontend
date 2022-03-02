39f473513599e1d12b2291be1a51c287d2ebb1ea
Zubair Sidhu <sidhuzubair@gmail.com>
2022-03-01 18:19:44 -0800

    Pass modal style to CommonModal

--- src/components/AdminProfilePage/DeleteAccountModal/DeleteAccountModal.jsx
+++ src/components/AdminProfilePage/DeleteAccountModal/DeleteAccountModal.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton } from '@chakra-ui/react';
-
+import CommonModal from '../../../common/CommonModal/CommonModal';
 import DeleteAccountModalContent from './DeleteAccountModalContent';
 import styles from './DeleteAccountModal.module.css';
 import FYALogo from '../../../assets/fya-logo-large.svg';
 
 // TODO:
-// - Move this component to src/components
 // - Replace ChakraUI Modal with common component once created
-// - Replace FYALogo with a less blurry svg
 // - Implement deleteAccount
 
 const deleteAccount = () => {
@@ -27,23 +24,17 @@ const DeleteAccountModal = ({ isOpen, onClose }) => {
   };
 
   return (
-    <Modal size="xl" isOpen={isOpen} onClose={closeModal} isCentered>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalCloseButton />
-        <ModalBody>
-          <div className={styles['delete-modal-content']}>
-            <img src={FYALogo} alt="Logo" className={styles['fya-logo']} />
-            <DeleteAccountModalContent
-              modalStep={modalStep}
-              setModalStep={setModalStep}
-              closeModal={closeModal}
-              deleteAccount={deleteAccount}
-            />
-          </div>
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+    <CommonModal isOpen={isOpen} onClose={closeModal} modalClassName={styles.modal}>
+      <div className={styles['delete-modal-content']}>
+        <img src={FYALogo} alt="Logo" className={styles['fya-logo']} />
+        <DeleteAccountModalContent
+          modalStep={modalStep}
+          setModalStep={setModalStep}
+          closeModal={closeModal}
+          deleteAccount={deleteAccount}
+        />
+      </div>
+    </CommonModal>
   );
 };
 
