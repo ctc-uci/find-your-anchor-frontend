fe9e25d59fd9cc917a28562698ae04fbf2cc5b03
Ashley Liu <aslorange61@gmail.com>
2022-03-06 18:43:21 -0800

    create ExportCSVRow component and add styling

--- src/components/ExportCSV/CSVPreview/CSVPreview.jsx
+++ src/components/ExportCSV/CSVPreview/CSVPreview.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
 
 import styles from './CSVPreview.module.css';
+import ExportCSVRow from '../ExportCSVRow/ExportCSVRow';
 
 const CSVPreview = ({ formValues }) => {
   return (
-    <div className={`${styles['csv-preview-wrapper']} ${styles['scrollable-div']}`}>
-      <p>CSV Preview</p>
-      {/* <pre>{JSON.stringify(formValues, null, 2)}</pre> */}
-      {formValues.length > 0 && (
-        <table className={styles['csv-table']}>
-          <thead>
-            <tr>
-              {/* display each property as a <th> tag */}
-              {/* eslint-disable react/no-array-index-key */}
-              {Object.keys(formValues[0]).map((property, index) => (
-                <th key={index}>{property}</th>
+    <div className="csv-preview">
+      <h2>CSV Preview</h2>
+      <div className={`${styles['csv-preview-wrapper']} ${styles['scrollable-div']}`}>
+        {/* <pre>{JSON.stringify(formValues, null, 2)}</pre> */}
+        {formValues.length > 0 && (
+          <Table className={styles['csv-preview']}>
+            <Thead>
+              <Tr>
+                {/* eslint-disable react/no-array-index-key */}
+                {Object.keys(formValues[0]).map((property, index) => (
+                  <Th key={index}>{property}</Th>
+                ))}
+              </Tr>
+            </Thead>
+            <Tbody>
+              {formValues.map(data => (
+                <ExportCSVRow key={data.box_id} data={data} />
               ))}
-            </tr>
-          </thead>
-          <tbody>
-            {formValues.map(data => (
-              <h1 key={data.box_id}>{data.box_id}</h1>
-            ))}
-          </tbody>
-        </table>
-      )}
+            </Tbody>
+          </Table>
+        )}
+      </div>
     </div>
   );
 };
