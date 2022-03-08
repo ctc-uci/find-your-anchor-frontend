dfd4e767c0fc0b932462f3fbc5f712744b38a27a
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-03-08 14:42:29 -0600

    login register form (#53)
    * Create PR for #50
    
    * created and styled login form
    
    * created and styled register form
    
    * fixed styling issues and replaced png logo with svg
    
    * added placeholder text to register form components
    
    Co-authored-by: dgardezi <dgardezi@users.noreply.github.com>
    Co-authored-by: dgardezi <dgardezi@gmail.com>
    Co-authored-by: Duron Gardezi <47585825+dgardezi@users.noreply.github.com>
    Co-authored-by: Sylvester Yue <56140648+sylvesteryue@users.noreply.github.com>

---
+++ src/components/Inputs/PasswordInput.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  FormControl,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Icon,
+  FormErrorMessage,
+  FormLabel,
+  FormHelperText,
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import styles from './Input.module.css';
+
+const PasswordInput = ({ error, register, showForgotPassword, title }) => {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <FormControl className={styles['form-control']} isInvalid={error}>
+      <FormLabel className={styles['form-title']}>{title}</FormLabel>
+      <InputGroup>
+        <Input
+          type={show ? 'text' : 'password'}
+          placeholder="Password must be at least 8 characters"
+          {...register}
+        />
+        <InputRightElement className={styles['password-show-hide']}>
+          <Button className={styles['password-show-hide-button']} size="sm" onClick={handleClick}>
+            {show ? <Icon as={ViewIcon} /> : <Icon as={ViewOffIcon} />}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <div className={styles['input-tools']}>
+        <div className={styles['form-error-wrapper']}>
+          {!error && <FormHelperText>&nbsp;</FormHelperText>}
+          <FormErrorMessage className={styles['form-error-message']}>
+            {error?.message}
+          </FormErrorMessage>
+        </div>
+        {showForgotPassword && (
+          <div className={styles['forgot-password-wrapper']} align="right">
+            <Link className={styles['forgot-password']} to="/forgotPassword">
+              Forgot Password?
+            </Link>
+          </div>
+        )}
+      </div>
+    </FormControl>
+  );
+};
+
+PasswordInput.propTypes = {
+  error: PropTypes.shape({ message: PropTypes.string }),
+  register: PropTypes.shape({ password: PropTypes.string }).isRequired,
+  showForgotPassword: PropTypes.bool,
+  title: PropTypes.string,
+};
+PasswordInput.defaultProps = {
+  error: null,
+  showForgotPassword: false,
+  title: 'Password',
+};
+
+export default PasswordInput;
