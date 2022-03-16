8dd94ee772f93363d8cdbd422279dec149add724
Ashley Liu <aslorange61@gmail.com>
2022-03-16 16:02:57 -0700

    change to CSS modules

--- src/pages/AddBoxForm/AddBoxFormPage.jsx
+++ src/pages/AddBoxForm/AddBoxFormPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import BoxForm from '../../components/AddBoxForm/AddBoxForm';
-import './AddBoxForm.css';
+import styles from './AddBoxForm.module.css';
 
 const AddBoxFormPage = () => {
   return (
     <ChakraProvider>
-      <div className="add-box-form">
-        <h1 className="add-box-form-title">Add Box</h1>
+      <div className={styles['add-box-form']}>
+        <h1 className={styles['add-box-form-title']}>Add Box</h1>
         <BoxForm />
       </div>
     </ChakraProvider>
