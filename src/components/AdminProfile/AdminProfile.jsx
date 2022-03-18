aa932bc907d806a43b401cd17ae916215a9b2b73
meghakak <meghakak1@gmail.com>
2022-03-18 16:24:38 -0700

    Connect backend to profile page.

--- src/components/AdminProfile/AdminProfile.jsx
+++ src/components/AdminProfile/AdminProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PropTypes, instanceOf } from 'prop-types';
 import { ChakraProvider, Button, useDisclosure, Input, Text } from '@chakra-ui/react';
 
@@ -9,24 +9,48 @@ import DeleteAccountModal from './DeleteAccountModal/DeleteAccountModal';
 import SendLinkModal from './SendLinkModal/SendLinkModal';
 import FYALogoLarge from '../../assets/fya-logo-large.svg';
 
-import { logout, useNavigate } from '../../common/auth_utils';
+import { logout, useNavigate, getCurrentUser, auth } from '../../common/auth_utils';
 import { Cookies, withCookies } from '../../common/cookie_utils';
+import { FYABackend } from '../../common/utils';
 
-const TextInput = ({ inputLabel, placeHolder, editable, editState, makeEditable }) => (
-  <div className={styles['form-input']}>
-    <Text>{inputLabel}</Text>
-    <div className={styles['editable-input']}>
-      <Input top="5px" size="lg" width="100%" placeholder={placeHolder} isDisabled={!editState} />
-      <button type="button" style={editable ? {} : { visibility: 'hidden' }} onClick={makeEditable}>
-        {editState ? (
-          <RiCheckFill color="#38a169" size={35} />
-        ) : (
-          <RiPencilFill color="#8E8E8E" size={35} />
-        )}
-      </button>
+const TextInput = ({
+  inputLabel,
+  placeHolder,
+  value,
+  setValue,
+  editable,
+  editState,
+  makeEditable,
+  handleCheckMarkClicked,
+}) => {
+  return (
+    <div className={styles['form-input']}>
+      <Text>{inputLabel}</Text>
+      <div className={styles['editable-input']}>
+        <Input
+          top="5px"
+          size="lg"
+          width="100%"
+          placeholder={placeHolder}
+          value={editState ? value : ''}
+          isDisabled={!editState}
+          onChange={e => setValue(e.target.value)}
+        />
+        <button
+          type="button"
+          style={editable ? {} : { visibility: 'hidden' }}
+          onClick={makeEditable}
+        >
+          {editState ? (
+            <RiCheckFill color="#38a169" size={35} onClick={handleCheckMarkClicked} />
+          ) : (
+            <RiPencilFill color="#8E8E8E" size={35} />
+          )}
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const AdminProfile = ({ cookies }) => {
   const {
@@ -43,6 +67,8 @@ const AdminProfile = ({ cookies }) => {
 
   const [editFirst, setEditFirst] = useState(false);
   const [editLast, setEditLast] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState();
@@ -55,6 +81,22 @@ const AdminProfile = ({ cookies }) => {
       console.log(errorMessage);
     }
   };
+
+  const handleCheckMarkClicked = async () => {
+    const user = await getCurrentUser(auth);
+    await FYABackend.put(`/users/${user.uid}`, {
+      firstName,
+      lastName,
+      userId: user.uid,
+    });
+  };
+
+  useEffect(async () => {
+    const firebaseUser = await getCurrentUser(auth);
+    const backendUser = await FYABackend.get(`/users/${firebaseUser.uid}`);
+    setFirstName(backendUser.data.user.first_name);
+    setLastName(backendUser.data.user.last_name);
+  }, []);
 
   return (
     <ChakraProvider>
@@ -78,17 +120,23 @@ const AdminProfile = ({ cookies }) => {
           </div>
           <TextInput
             inputLabel="First Name"
-            placeHolder="Jane"
+            placeHolder={firstName}
+            value={firstName}
+            setValue={setFirstName}
             editable
             editState={editFirst}
             makeEditable={() => setEditFirst(!editFirst)}
+            handleCheckMarkClicked={handleCheckMarkClicked}
           />
           <TextInput
             inputLabel="Last Name"
-            placeHolder="Doe"
+            placeHolder={lastName}
+            value={lastName}
+            setValue={setLastName}
             editable
             editState={editLast}
             makeEditable={() => setEditLast(!editLast)}
+            handleCheckMarkClicked={handleCheckMarkClicked}
           />
           <TextInput inputLabel="Email" placeHolder="name@findyouranchor.us" editState={false} />
         </div>
@@ -117,14 +165,20 @@ TextInput.defaultProps = {
   editable: false,
   editState: false,
   makeEditable: null,
+  value: null,
+  setValue: null,
+  handleCheckMarkClicked: null,
 };
 
 TextInput.propTypes = {
   inputLabel: PropTypes.string.isRequired,
   placeHolder: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  setValue: PropTypes.func,
   editable: PropTypes.bool,
   editState: PropTypes.bool,
   makeEditable: PropTypes.func,
+  handleCheckMarkClicked: PropTypes.func,
 };
 
 AdminProfile.propTypes = {
