6e086b87f522b4ce44721dfb642bf47c979b2724
Sylvester Yue <sylvesteryue2301@gmail.com>
2022-03-20 23:04:32 -0700

    make common auth layout component for all auth pages

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import AuthPageLayout from '../../components/AuthPageLayout/AuthPageLayout';
 import ForgotPasswordForm from '../../components/ForgotPassword/ForgotPasswordForm';
-import styles from './ForgotPasswordPage.module.css';
-import FYALogo from '../../assets/fya-text-logo.svg';
 
-const ForgotPasswordPage = () => {
-  return (
-    <ChakraProvider>
-      <div className={styles['page-container']}>
-        <div className={styles['forgot-password-form']}>
-          <div className={styles['forgot-password-form-container']}>
-            <div className={styles['logo-container']}>
-              <img src={FYALogo} className={styles.logo} alt="logo" />
-            </div>
-            <div className={styles['forgot-password-form-component']}>
-              <ForgotPasswordForm />
-            </div>
-          </div>
-        </div>
-        <div className={styles.banner} />
-      </div>
-    </ChakraProvider>
-  );
-};
+const ForgotPasswordPage = () => (
+  <AuthPageLayout>
+    <ForgotPasswordForm />
+  </AuthPageLayout>
+);
 
 export default ForgotPasswordPage;
--- src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import AuthPageLayout from '../../components/AuthPageLayout/AuthPageLayout';
 import ResetPasswordForm from '../../components/ResetPassword/ResetPasswordForm';
-import styles from './ResetPasswordPage.module.css';
-import FYALogo from '../../assets/fya-text-logo.svg';
 
-const LoginPage = () => {
-  return (
-    <ChakraProvider>
-      <div className={styles['page-container']}>
-        <div className={styles['reset-password-form']}>
-          <div className={styles['reset-password-container']}>
-            <div className={styles['logo-container']}>
-              <img src={FYALogo} className={styles.logo} alt="logo" />
-            </div>
-            <div className={styles['reset-password-form-component']}>
-              <ResetPasswordForm />
-            </div>
-          </div>
-        </div>
-        <div className={styles.banner} />
-      </div>
-    </ChakraProvider>
-  );
-};
+const ResetPasswordPage = () => (
+  <AuthPageLayout>
+    <ResetPasswordForm />
+  </AuthPageLayout>
+);
 
-export default LoginPage;
+export default ResetPasswordPage;
