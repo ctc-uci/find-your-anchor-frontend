bbc9ae4dffd131834cabde6558567836f352c9b7
Ashley Liu <aslorange61@gmail.com>
2022-03-23 16:57:53 -0700

    change to CSS modules

--- src/components/AddBoxForm/DropZone/AddBoxDropZone.jsx
+++ src/components/AddBoxForm/DropZone/AddBoxDropZone.jsx
@@ -5,7 +5,7 @@ import { useDropzone } from 'react-dropzone';
 import { CloseIcon } from '@chakra-ui/icons';
 import PropTypes from 'prop-types';
 import { BsUpload } from 'react-icons/bs';
-import './AddBoxDropZone.css';
+import styles from './AddBoxDropZone.module.css';
 
 const AddBoxDropZone = ({ setFiles }) => {
   const { getRootProps, getInputProps, isDragAccept, isDragReject, acceptedFiles, open } =
@@ -25,10 +25,13 @@ const AddBoxDropZone = ({ setFiles }) => {
   });
 
   const dropzoneBox = useMemo(() => {
-    let base = 'dropzone-zone';
-    base += isDragAccept ? ' dropzone-accept' : '';
-    base += isDragReject ? ' dropzone-reject' : '';
-    return base;
+    if (isDragAccept) {
+      return 'dropzone-accept';
+    }
+    if (isDragReject) {
+      return 'dropzone-reject';
+    }
+    return '';
   }, [isDragReject, isDragAccept]);
 
   const removeUploadedPhoto = () => {
@@ -38,34 +41,34 @@ const AddBoxDropZone = ({ setFiles }) => {
   };
 
   const acceptedFileItems = acceptedFiles.map(acceptedFile => (
-    <li key={acceptedFile.path} className="file-item">
+    <li key={acceptedFile.path} className={styles['file-item']}>
       <button
-        className="file-item-span"
+        className={styles['file-item-span']}
         type="button"
         aria-label="Remove"
         onClick={removeUploadedPhoto}
       >
         <CloseIcon w={3} h={3} color="gray.600" />
       </button>
-      <span className="file-item-span-text">{acceptedFile.path}</span>
+      <span className={styles['file-item-span-text']}>{acceptedFile.path}</span>
     </li>
   ));
 
   return (
     <>
       {!uploaded ? (
-        <div className={dropzoneBox} {...getRootProps()}>
+        <div className={`${styles['dropzone-zone']} ${styles[dropzoneBox]}`} {...getRootProps()}>
           <input {...getInputProps()} />
-          <BsUpload className="dropzone-icon" />
-          <button type="button" className="file-selector" onClick={open}>
+          <BsUpload className={styles['dropzone-icon']} />
+          <button type="button" className={styles['file-selector']} onClick={open}>
             Click
           </button>
-          <span className="dropzone-text">/drag file to upload</span>
-          <p className="dropzone-support-text">Support for jpeg, jpg, png</p>
+          <span className={styles['dropzone-text']}>/drag file to upload</span>
+          <p className={styles['dropzone-support-text']}>Support for jpeg, jpg, png</p>
         </div>
       ) : (
         <div>
-          <ul className="files-list">{acceptedFileItems}</ul>
+          <ul className={styles['files-list']}>{acceptedFileItems}</ul>
         </div>
       )}
     </>
