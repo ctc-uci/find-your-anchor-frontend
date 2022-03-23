d019f4a3ad94139083f15f5cee8b2ac72725b5ca
meghakak <meghakak1@gmail.com>
2022-03-22 17:18:00 -0700

    Update initials on navbar to use user's initials.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import styles from './NavBar.module.css';
 import FYALogo from '../../assets/fya-logo.png';
-import PlaceHolderPFP from '../../assets/placeholder_pfp.svg';
+
+import { FYABackend } from '../../common/utils';
+import { getCurrentUser, auth } from '../../common/auth_utils';
 
 const AdminLinks = () => (
   <>
@@ -21,20 +23,32 @@ const UserLinks = () => (
   </>
 );
 
+const getUserInitials = async () => {
+  const user = await getCurrentUser(auth);
+  const backendUser = await FYABackend.get(`/users/userId/${user.uid}`);
+  return (
+    (backendUser.data.user.first_name ? backendUser.data.user.first_name[0] : '') +
+    (backendUser.data.user.last_name ? backendUser.data.user.last_name[0] : '')
+  );
+};
+
 const NavBar = ({ isAdmin }) => {
+  const [initials, setInitials] = useState('');
+
+  useEffect(async () => {
+    const temp = await getUserInitials();
+    setInitials(temp);
+  }, []);
+
   return (
     <div className={styles['nav-bar']}>
       <Link to="/">
-        <div className={styles['fya-logo']}>
-          <img src={FYALogo} alt="Find Your Anchor Logo" />
-        </div>
+        <img src={FYALogo} alt="Find Your Anchor Logo" />
       </Link>
       <div className={styles['navbar-buttons-and-account']}>
         <div className={styles['navbar-buttons']}>{isAdmin ? <AdminLinks /> : <UserLinks />}</div>
         <Link to="/profile">
-          <div className={styles['navbar-account']}>
-            <img className={styles['profile-picture']} src={PlaceHolderPFP} alt="Profile" />
-          </div>
+          <div className={styles['navbar-initials']}>{initials}</div>
         </Link>
       </div>
     </div>
