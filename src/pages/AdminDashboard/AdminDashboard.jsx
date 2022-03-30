569310d0a1ea3dd2e9d4c11356933f2b754cc197
jennymylee <jennylee038@gmail.com>
2022-03-29 18:05:12 -0700

    changed css to module.css

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { ChakraProvider, Button } from '@chakra-ui/react';
-
+import styles from './AdminDashboard.module.css';
 import Map from '../../components/Map/Map';
 import BoxApproval from '../../components/BoxApproval/BoxApproval';
 import AdminMarkerInfo from '../../components/AdminMarkerInfo/AdminMarkerInfo';
-import './AdminDashboard.css';
 
 const AdminDashboard = () => {
   // This state determines whether or not to show the admin approval (left) side bar
@@ -23,24 +22,24 @@ const AdminDashboard = () => {
   const [selectedBox, setSelectedBox] = useState(null);
   return (
     <ChakraProvider>
-      <div className="admin-dashboard-container">
-        <div className="side-bar-and-map-container">
-          <div className={`side-bar ${showReview ? 'show-review' : ''}`}>
+      <div className={styles['admin-dashboard-container']}>
+        <div className={styles['side-bar-and-map-container']}>
+          <div className={`${styles['side-bar']} ${showReview ? styles['show-review'] : ''}`}>
             <BoxApproval />
             <Button
               variant="link"
               colorScheme="white"
-              className="close-button"
+              className={styles['close-button']}
               onClick={() => setShowReview(false)}
             >
               Close
             </Button>
           </div>
           <div
-            className={`map
-            ${showReview && !selectedZipCode ? 'one-bar-open' : ''}
-            ${selectedZipCode && !showReview ? 'one-bar-open' : ''}
-            ${selectedZipCode && showReview ? 'two-bars-open' : ''}`}
+            className={`${styles.map}
+            ${showReview && !selectedZipCode ? styles['one-bar-open'] : ''}
+            ${selectedZipCode && !showReview ? styles['one-bar-open'] : ''}
+            ${selectedZipCode && showReview ? styles['two-bars-open'] : ''}`}
           >
             <Map
               setSelectedZipCode={setSelectedZipCode}
@@ -53,12 +52,18 @@ const AdminDashboard = () => {
 
           <Button
             colorScheme="blue"
-            className={`review-submission-button ${showReview ? 'show-review' : ''}`}
+            className={`${styles['review-submission-button']} ${
+              showReview ? styles['show-review'] : ''
+            }`}
             onClick={() => setShowReview(true)}
           >
             Review Submission
           </Button>
-          <div className={`side-bar ${selectedZipCode && selectedCountry ? 'show-info' : ''}`}>
+          <div
+            className={`${styles['side-bar']} ${
+              selectedZipCode && selectedCountry ? styles['show-info'] : ''
+            }`}
+          >
             <AdminMarkerInfo
               selectedZipCode={selectedZipCode}
               selectedCountry={selectedCountry}
