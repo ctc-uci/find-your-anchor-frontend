48344a86b9c75542b5baac2319589f95eacb8d06
Kyle Pan <kylerpan691@gmail.com>
2022-04-02 22:32:21 -0700

    sort by values effect the rows

--- src/components/ExportCSV/ExportCSVForm/ExportCSVForm.jsx
+++ src/components/ExportCSV/ExportCSVForm/ExportCSVForm.jsx
@@ -156,6 +156,32 @@ const ExportCSVForm = ({ formID, setFormValues }) => {
       },
     });
 
+    res.data.forEach((element, index) => {
+      if (element.date.length !== 10) {
+        res.data[index].date = `0${element.date}`;
+      }
+    });
+
+    if (data.sortBy === 'ascend-box-num') {
+      res.data.sort((a, b) => a.box_id - b.box_id);
+    } else if (data.sortBy === 'descend-box-num') {
+      res.data.sort((a, b) => b.box_id - a.box_id);
+    } else if (data.sortBy === 'ascend-zip-code') {
+      res.data.sort((a, b) => a.zip_code - b.zip_code);
+    } else if (data.sortBy === 'descend-zip-code') {
+      res.data.sort((a, b) => b.zip_code - a.zip_code);
+    } else {
+      res.data.sort((a, b) => {
+        if (a.date > b.date) {
+          return 1;
+        }
+        if (a.date < b.date) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+
     setFormValues(res.data);
 
     navigate('/export-csv-preview', { state: { rows: res.data } });
