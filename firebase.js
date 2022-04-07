111acffefd5e1708e17f148dc65b4687f9dd5999
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-07 15:49:46 -0700

    Switch from firebase-adminsdk.json to env vars

--- firebase.js
+++ firebase.js
@@ -2,8 +2,16 @@ const admin = require('firebase-admin');
 
 require('dotenv').config();
 
-const credentials = require('./firebase-adminsdk.json');
+// const credentials = require('./firebase-adminsdk.json');
 
-admin.initializeApp({ credential: admin.credential.cert(credentials) });
+// admin.initializeApp({ credential: admin.credential.cert(credentials) });
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
 
 module.exports = admin;
