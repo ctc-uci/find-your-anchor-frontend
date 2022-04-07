079b0bdc1955edac628fef67ee0788254f1c8c47
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-07 16:19:36 -0700

    change frontend path

--- app.js
+++ app.js
@@ -43,11 +43,11 @@ app.use('/adminInvite', adminInviteRouter);
 
 // HEROKU TESTING
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../frontend/build/public/index.html'));
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
 });
 // HEROKU TESTING
 
