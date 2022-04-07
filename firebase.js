055b223fa8ba6caaab4e0e52c0a610751e8750f4
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-07 15:56:14 -0700

    Replace newlines in firebase private key

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
