665e318e7aef03c2d883425def8d8796612ef4d9
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-07 16:10:15 -0700

    Change index.html path

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '/../frontend/build/public/index.html'));
 });
 // HEROKU TESTING
 
