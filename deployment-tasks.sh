f3772da408ef16012cbf5139ba6d4d297f76aa22
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-09 17:42:38 -0700

    Actually add release script

---
+++ deployment-tasks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Print env vars
+printenv
+
+# Clones frontend PR branch
+git clone --branch $HEROKU_BRANCH $FRONTEND_CLONE
+
+# Clones backend branch
+git clone --branch dev $BACKEND_CLONE
+
+echo "done git clone"
