ef09f0bde3e05f4f5aa1f91294b1f1fab0741f10
jeffcheema <jeffcheema007@gmail.com>
2022-04-17 10:59:28 -0700

    Added mobile view for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { ChakraProvider, Button, useDisclosure } from '@chakra-ui/react';
-import UploadCSV from '../UploadCSV/UploadCSV';
+import {
+  ChakraProvider,
+  useDisclosure,
+  IconButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  SimpleGrid,
+  Box,
+  Icon,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import {
+  FaFileUpload,
+  FaPlusSquare,
+  FaFileCsv,
+  FaUserAlt,
+  FaInfoCircle,
+  FaSearchLocation,
+} from 'react-icons/fa';
 
+import UploadCSV from '../UploadCSV/UploadCSV';
+import useMobileWidth from '../../common/useMobileWidth';
 import styles from './NavBar.module.css';
 import FYALogo from '../../assets/fya-logo.png';
 
@@ -12,6 +35,7 @@ import { getCurrentUser, auth } from '../../common/auth_utils';
 
 const NavBar = ({ isAdmin }) => {
   const [initials, setInitials] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const {
     isOpen: isUploadCSVOpenModal,
@@ -33,59 +57,85 @@ const NavBar = ({ isAdmin }) => {
       const temp = await getUserInitials();
       setInitials(temp);
     }
-  }, [isAdmin]);
+  }, [isAdmin, useMobileWidth]);
 
   const AdminLinks = () => (
     <>
-      <NavLink
-        to="/add-box-form"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Add Box
-      </NavLink>
-      <Button variant="unstyled" onClick={onUploadCSVOpenModal} className={styles['upload-button']}>
-        Upload CSV
+      <Box>
+        <NavLink
+          onClick={onClose}
+          to="/profile"
+          className={navLink =>
+            navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
+          }
+        >
+          <div> {useMobileWidth() && <Icon as={FaUserAlt} />} Profile</div>
+        </NavLink>
+      </Box>
+      <Box>
+        <NavLink
+          onClick={onClose}
+          to="/add-box-form"
+          className={navLink =>
+            navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
+          }
+        >
+          {useMobileWidth() && <Icon as={FaPlusSquare} />}Add Box
+        </NavLink>
+      </Box>
+      <Box onClick={onUploadCSVOpenModal}>
+        {useMobileWidth() && <Icon as={FaFileUpload} />} Upload CSV
         <UploadCSV isOpen={isUploadCSVOpenModal} onClose={onCloseUploadCSVOpenModal} />
-      </Button>
-      <NavLink
-        to="/export-csv"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Export CSV
-      </NavLink>
+      </Box>
+      <Box>
+        <NavLink
+          onClick={onClose}
+          to="/export-csv"
+          className={navLink =>
+            navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
+          }
+        >
+          {useMobileWidth() && <Icon as={FaFileCsv} />} Export CSV
+        </NavLink>
+      </Box>
     </>
   );
 
   const UserLinks = () => (
     <>
-      <NavLink
-        to="/about"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/relocate-box-form"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Launch a Box
-      </NavLink>
-      <NavLink
-        to="/pickup-box-form"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Found a Box
-      </NavLink>
+      <Box>
+        <NavLink
+          onClick={onClose}
+          to="/about"
+          className={navLink =>
+            navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
+          }
+        >
+          {useMobileWidth() && <Icon as={FaInfoCircle} />} About
+        </NavLink>
+      </Box>
+      <Box>
+        <NavLink
+          onClick={onClose}
+          to="/relocate-box-form"
+          className={navLink =>
+            navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
+          }
+        >
+          {useMobileWidth() && <Icon as={FaPlusSquare} />} Launch a Box
+        </NavLink>
+      </Box>
+      <Box>
+        <NavLink
+          onClick={onClose}
+          to="/pickup-box-form"
+          className={navLink =>
+            navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
+          }
+        >
+          {useMobileWidth() && <Icon as={FaSearchLocation} />} Found a Box
+        </NavLink>
+      </Box>
     </>
   );
 
@@ -98,14 +148,39 @@ const NavBar = ({ isAdmin }) => {
           </div>
         </NavLink>
         <div className={styles['navbar-buttons-and-account']}>
-          <div className={styles['navbar-buttons']}>{isAdmin ? <AdminLinks /> : <UserLinks />}</div>
-          {isAdmin && (
+          {!useMobileWidth() && (
+            <div className={styles['navbar-buttons']}>
+              {isAdmin ? <AdminLinks /> : <UserLinks />}
+            </div>
+          )}
+          {!useMobileWidth() && isAdmin && (
             <NavLink to="/profile">
               <div className={styles['navbar-initials']}>{initials}</div>
             </NavLink>
           )}
+          {useMobileWidth() && (
+            <IconButton
+              onClick={onOpen}
+              className={styles['hamburger-icon']}
+              icon={<HamburgerIcon />}
+            />
+          )}
         </div>
       </div>
+      <Modal size="full" isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent className={styles['nav-modal']}>
+          <ModalHeader>Menu</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className={styles['']}>
+              <SimpleGrid minChildWidth="200px" spacing="40px">
+                {isAdmin ? <AdminLinks /> : <UserLinks />}
+              </SimpleGrid>
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </ChakraProvider>
   );
 };
