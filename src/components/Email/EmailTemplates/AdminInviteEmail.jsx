c4f157ff32d197b5856f18068834f5477d980722
meghakak <meghakak1@gmail.com>
2022-04-26 13:29:46 -0700

    Create initial email template.

--- src/components/Email/EmailTemplates/AdminInviteEmail.jsx
+++ src/components/Email/EmailTemplates/AdminInviteEmail.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Email, Item } from 'react-html-email';
+import { Email } from 'react-html-email';
 
 const AdminInviteEmail = ({ url }) => {
   return (
-    <Email>
-      <Item>{url}</Item>
+    <Email style={{ float: 'left' }}>
+      <p style={{ color: 'black' }}>Hi,</p>
+      <p style={{ color: 'black' }}>
+        [INSERT SENDER NAME] is inviting you to register for the Find Your Anchor Launch Map. Please
+        click the link below to create an account using your Find Your Anchor email address.
+      </p>
+      <p style={{ color: 'black' }}>
+        <a href={url}>Registration Link</a>
+      </p>
+      <p style={{ color: 'black' }}>
+        Thank you,
+        <br />
+        Find Your Anchor
+      </p>
     </Email>
   );
 };
