0f4e046b125f4d108ec0aaee7d76b376d4ceb4c0
jeffcheema <jeffcheema007@gmail.com>
2022-05-03 19:33:16 -0700

    Fixed mobile view for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { ChakraProvider, Button, useDisclosure } from '@chakra-ui/react';
+import PropTypes, { instanceOf } from 'prop-types';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
+import {
+  ChakraProvider,
+  useDisclosure,
+  Link,
+  Box,
+  Flex,
+  Spacer,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  IconButton,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
 import UploadCSV from '../UploadCSV/UploadCSV';
-
 import styles from './NavBar.module.css';
 import FYALogo from '../../assets/fya-logo.png';
-
 import { FYABackend } from '../../common/utils';
-import { getCurrentUser, auth } from '../../common/auth_utils';
-
+import { getCurrentUser, auth, logout } from '../../common/auth_utils';
+import { Cookies, withCookies } from '../../common/cookie_utils';
 import useMobileWidth from '../../common/useMobileWidth';
+import addBoxIcon from '../../assets/navBarIcons/add-box-icon.svg';
+import adminDashboardIcon from '../../assets/navBarIcons/admin-dashboard-icon.svg';
+import exportCSVIcon from '../../assets/navBarIcons/export-csv-icon.svg';
+import hamburgerIcon from '../../assets/navBarIcons/hamburger-icon.svg';
+import uploadCSVIcon from '../../assets/navBarIcons/upload-csv-icon.svg';
+import userProfileIcon from '../../assets/navBarIcons/user-profile-icon.svg';
+import logoutIcon from '../../assets/navBarIcons/logout-icon.svg';
 
-const NavBar = ({ isAdmin }) => {
+const NavBar = ({ isAdmin, cookies }) => {
   const isMobile = useMobileWidth();
   const [initials, setInitials] = useState('');
   const navigate = useNavigate();
+  const location = useLocation();
 
   const {
     isOpen: isUploadCSVOpenModal,
     onOpen: onUploadCSVOpenModal,
     onClose: onCloseUploadCSVOpenModal,
   } = useDisclosure();
-
-  const onCSVUpload = () => {
-    if (!isMobile) {
-      onUploadCSVOpenModal();
-    } else {
-      navigate('/upload-csv');
-    }
-  };
-
+  const {
+    isOpen: isMobileNavOpen,
+    onOpen: onMobileNavOpen,
+    onClose: onMobileNavClose,
+  } = useDisclosure();
   const getUserInitials = async () => {
     const user = await getCurrentUser(auth);
     const backendUser = await FYABackend.get(`/users/userId/${user.uid}`);
@@ -39,103 +54,148 @@ const NavBar = ({ isAdmin }) => {
       (backendUser.data.user.last_name ? backendUser.data.user.last_name[0] : '')
     );
   };
-
   useEffect(async () => {
     if (isAdmin) {
       const temp = await getUserInitials();
       setInitials(temp);
     }
-  }, [isAdmin]);
+    onCloseUploadCSVOpenModal();
+  }, [isAdmin, location]);
 
-  const AdminLinks = () => (
-    <>
-      <NavLink
-        to="/"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
+  const linkDesktopElement = (linkData, hasIcon = false) => {
+    return (
+      <Link
+        as={linkData.path && NavLink}
+        to={linkData?.path}
+        onClick={linkData?.onClick}
+        className={styles['nav-link']}
+        color={location.pathname === linkData.path ? '#126893' : 'black'}
+        marginLeft={hasIcon && isMobile ? '5%' : isMobile && 0}
       >
-        Admin Dashboard
-      </NavLink>
-      <NavLink
-        to="/add-box-form"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Add Box
-      </NavLink>
-      <Button variant="unstyled" onClick={onCSVUpload} className={styles['upload-button']}>
-        Upload CSV
-        <UploadCSV isOpen={isUploadCSVOpenModal} onClose={onCloseUploadCSVOpenModal} />
-      </Button>
-      <NavLink
-        to="/export-csv"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Export CSV
-      </NavLink>
-    </>
+        {linkData.display}
+      </Link>
+    );
+  };
+  const linkMobileElement = linkData => (
+    <Flex direction="row" p="5">
+      <img src={linkData.icon} alt="" /> {linkDesktopElement(linkData, true && linkData.icon)}
+    </Flex>
   );
+  const generalUserLinks = [
+    {
+      path: '/about',
+      display: 'About',
+    },
+    {
+      path: '/launch-box-form',
+      display: 'Launch a Box',
+    },
+    {
+      path: '/found-box-form',
+      display: 'Found a Box',
+    },
+  ];
+  const adminLinks = [
+    {
+      path: '/',
+      display: 'Admin Dashboard',
+      icon: adminDashboardIcon,
+    },
+    {
+      path: '/add-box-form',
+      display: 'Add Box',
+      icon: addBoxIcon,
+    },
+    {
+      display: 'Upload CSV',
+      onClick: () => {
+        if (!isMobile) {
+          onUploadCSVOpenModal();
+        } else {
+          navigate('/upload-csv');
+        }
+      },
+      icon: uploadCSVIcon,
+    },
+    {
+      path: '/export-csv',
+      display: 'Export CSV',
+      icon: exportCSVIcon,
+    },
+    {
+      path: '/profile',
+      display: isMobile ? (
+        'User Profile'
+      ) : (
+        <div className={styles['navbar-initials']}>{initials}</div>
+      ),
+      icon: userProfileIcon,
+    },
+    {
+      display: 'Log out',
+      onClick: async () => {
+        await logout(cookies);
+        navigate('/login');
+      },
+      icon: logoutIcon,
+      mobileOnly: true,
+    },
+  ];
+  const desktopNavElements = isAdmin
+    ? adminLinks.map(link => (link.mobileOnly ? <></> : linkDesktopElement(link)))
+    : generalUserLinks.map(link => (link.mobileOnly ? <></> : linkDesktopElement(link)));
 
-  const UserLinks = () => (
-    <>
-      <NavLink
-        to="/about"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/launch-box-form"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Launch a Box
-      </NavLink>
-      <NavLink
-        to="/found-box-form"
-        className={navLink =>
-          navLink.isActive ? styles['nav-link-selected'] : styles['nav-link-unselected']
-        }
-      >
-        Found a Box
-      </NavLink>
-    </>
-  );
-
+  const mobileNavElements = isAdmin
+    ? adminLinks.map(link => (link.desktopOnly ? <></> : linkMobileElement(link)))
+    : generalUserLinks.map(link => (link.desktopOnly ? <></> : linkMobileElement(link)));
   return (
     <ChakraProvider>
-      <div className={styles['nav-bar']}>
-        <NavLink to="/">
-          <div className={styles['fya-logo']}>
-            <img src={FYALogo} alt="Find Your Anchor Logo" />
-          </div>
-        </NavLink>
-        <div className={styles['navbar-buttons-and-account']}>
-          <div className={styles['navbar-buttons']}>{isAdmin ? <AdminLinks /> : <UserLinks />}</div>
-          {isAdmin && (
-            <NavLink to="/profile">
-              <div className={styles['navbar-initials']}>{initials}</div>
-            </NavLink>
+      <Flex minWidth="100%" boxShadow="md" alignItems="center" gap="2">
+        <Box p="1">
+          <Link as={NavLink} to="/">
+            <div className={styles['fya-logo']}>
+              <img src={FYALogo} alt="Find Your Anchor Logo" />
+            </div>
+          </Link>
+        </Box>
+        <Spacer />
+        <Box
+          className={styles['nav-box']}
+          maxWidth={isAdmin ? '533.25px' : '350px'}
+          p={isMobile && 2}
+        >
+          {isMobile ? (
+            <IconButton
+              onClick={onMobileNavOpen}
+              colorScheme="gray"
+              aria-label="Navigation"
+              icon={<img src={hamburgerIcon} alt="" />}
+            />
+          ) : (
+            desktopNavElements
           )}
-        </div>
-      </div>
+        </Box>
+      </Flex>
+      <Modal isOpen={isMobileNavOpen && isMobile} onClose={onMobileNavClose} size="full">
+        <ModalOverlay />
+        <ModalContent className={styles['nav-modal']}>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex direction="column" p="5">
+              {mobileNavElements}
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <UploadCSV isOpen={isUploadCSVOpenModal} onClose={onCloseUploadCSVOpenModal} />
     </ChakraProvider>
   );
 };
-
 NavBar.defaultProps = {
   isAdmin: false,
 };
-
 NavBar.propTypes = {
   isAdmin: PropTypes.bool,
+  cookies: instanceOf(Cookies).isRequired,
 };
-
-export default NavBar;
+export default withCookies(NavBar);
