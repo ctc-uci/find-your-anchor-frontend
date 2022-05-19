994610a21b6008f305eb42c924a7c33faac375e5
meghakak <meghakak1@gmail.com>
2022-05-19 16:22:30 -0700

    Revert auth page fix - already fixed in another PR.

--- src/components/AuthPageLayout/AuthPageLayout.jsx
+++ src/components/AuthPageLayout/AuthPageLayout.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
-
 import PropTypes from 'prop-types';
 import styles from './AuthPageLayout.module.css';
 import FYALogo from '../../assets/fya-text-logo.svg';
 import FYALogoMobile from '../../assets/fya-logo-large.svg';
 
 const AuthPageLayout = ({ children, backgroundImage }) => {
-  const navigate = useNavigate();
-
   return (
     <ChakraProvider>
       <div className={styles['page-container']}>
         <div className={styles['auth-form']}>
           <div className={styles['auth-container']}>
             <div className={styles['logo-container']}>
-              <button type="button" onClick={() => navigate('/')}>
-                <img src={FYALogo} className={styles.logo} alt="logo" />
-              </button>
+              <img src={FYALogo} className={styles.logo} alt="logo" />
             </div>
             <div className={styles['auth-form-component']}>{children}</div>
           </div>
