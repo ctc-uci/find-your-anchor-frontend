9b38029d832103fb0337873f67e048ab72a147f6
meghakak <meghakak1@gmail.com>
2022-06-17 19:17:48 -0700

    Changed font.

--- src/common/ChakraTheme.jsx
+++ src/common/ChakraTheme.jsx
@@ -20,27 +20,27 @@ const ChakraTheme = extendTheme({
   },
   textStyles: {
     'header-1': {
-      fontFamily: 'Inter, sans-serif',
+      fontFamily: 'Verdana, sans-serif',
       fontSize: '36px',
       fontWeight: 700,
     },
     'header-2': {
-      fontFamily: 'Inter, sans-serif',
+      fontFamily: 'Verdana, sans-serif',
       fontSize: '28px',
       fontWeight: 400,
     },
     subheader: {
-      fontFamily: 'Inter, sans-serif',
+      fontFamily: 'Verdana, sans-serif',
       fontSize: '20px',
       fontWeight: 400,
     },
     body: {
-      fontFamily: 'Inter, sans-serif',
+      fontFamily: 'Verdana, sans-serif',
       fontSize: '16px',
       fontWeight: 400,
     },
     descriptions: {
-      fontFamily: 'Inter, sans-serif',
+      fontFamily: 'Verdana, sans-serif',
       fontSize: '14px',
       fontWeight: 400,
     },
